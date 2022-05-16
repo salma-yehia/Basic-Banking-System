bffc75fb9f3859382f8707235f8f99441ab2728e
salma-yehia <73062191+salma-yehia@users.noreply.github.com>
2022-05-16 19:49:42 +0200

    Add files via upload

---
+++ Bank/fromTo.php
@@ -0,0 +1,29 @@
+<?php include "includes/header.php" ?>
+
+        <title>Transfer History</title>
+    </header>
+    <body>
+    <h1>Transfer History</h1>
+    <table>
+            <tr>
+                <th>From</th>
+                <th>To</th>
+                <th>Amount</th>
+                <th>Date and Time</th>
+            </tr>
+            <?php
+                $sql = "SELECT * FROM send_money";
+                $result = $conn->query($sql);
+                if(!$result){
+                    die("Invalid query: " . $conn->error);
+                }
+                while($row = $result->fetch_assoc()){
+                    echo "<tr>
+                            <td>" . $row["sender"] . "</td>
+                            <td>" . $row["receiver"] . "</td>
+                            <td>" . $row["balance"] . "</td>
+                            <td>" . $row["time"] . "</td>
+                        </tr>";
+                }
+            ?>                
+        </table>
---
+++ Bank/home.php
@@ -0,0 +1,20 @@
+<?php include "includes/header.php" ?>
+
+        <title>Home</title>
+        <style>
+            body {
+                background-image: url(layout/images/bank.jpg);
+                background-repeat: no-repeat;
+                background-size: cover;
+                }
+        </style>
+    </header>
+    <body>
+        <h1><b>Welcome</b></h1>
+        <p><b>A bank is a financial institution licensed to receive deposits and make loans. Banks may also provide financial services such as wealth management, currency exchange, and safe deposit boxes. There are several different kinds of banks including retail banks, commercial or corporate banks, and investment banks. In most countries, banks are regulated by the national government or central bank.</b></p>
+        <div class="view"><a href="customers.php">VIEW CUSTOMERS</a></div>
+        <script src="layout/js/jquery.min.js"></script>
+        <script src="layout/js/bootstrap.min.js"></script>
+        <script src="layout/js/backend.js"></script>
+    </body>
+</html>        
